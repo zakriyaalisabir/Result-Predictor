8b3c7f5e334ca9d55943869f6b5acd707bd3886b
zakriyaalisabir <zakriyaalisabir10@gmail.com>
2018-10-26 23:59:16 +0500

    updated firebase account

--- public/assets/js/firebase/firebase.js
+++ public/assets/js/firebase/firebase.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCnr67Ts3aDssPDSDpVOuqmQKIuUffNSuw",
-    authDomain: "result-predictor.firebaseapp.com",
-    databaseURL: "https://result-predictor.firebaseio.com",
-    projectId: "result-predictor",
-    storageBucket: "",
-    messagingSenderId: "211040294283"
-};
+    apiKey: "AIzaSyBNtNSTQdWo3zEgDtOtPl_Hqkb_Sjzf-0U",
+    authDomain: "result-predictor-bc65f.firebaseapp.com",
+    databaseURL: "https://result-predictor-bc65f.firebaseio.com",
+    projectId: "result-predictor-bc65f",
+    storageBucket: "result-predictor-bc65f.appspot.com",
+    messagingSenderId: "830356096475"
+  };
 firebase.initializeApp(config);
